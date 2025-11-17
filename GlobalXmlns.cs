4ad97c6bce79a359a88ac5c275461efc60c0aa61
literalmentesteven <stivencuadra0801@gmail.com>
2025-11-16 19:05:10 -0600

    Delete GlobalXmlns.cs

--- GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MediSync")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MediSync.Pages")]
